417d0288896634c6147be56e585acf35dc137744
pablolimas <pablolimas@gmail.com>
2015-10-20 21:17:37 -0200

    Novas imagens

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <html>
     <head>
-        <title>FARMACIAS RAIGA</title>
+        <title>MATERIAIS DE INFORMÁTICA</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
@@ -9,32 +9,67 @@
     </head>
 
     <body>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="alert alert-success" role="alert" style="text-align:center " > <h1 style="color:orangered" >DROGARIA RAIGA</h1></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="alert alert-success" role="alert" style="text-align:center " > <h1 style="color:orangered" >LIVRARIA DO ADVOGADO</h1></div>
+                </div>
             </div>
-        </div>
 
 
-        <div class="row">
-            <div class="col-lg-12" >
-                <ul class="nav nav-tabs">
-                    <li class="active">
-                        <a href="#">HOME</a></li>
-                    <li><a href="#">PRODUTOS</a></li>
-                    <li><a href="#">CARRINHO DE COMPRAS</a></li>
-                    <li><a href="#">LOGIN</a></li>
-                    <li><a href="#">PERFIL</a></li>
-                </ul>
+            <div class="row"  style="background-color:#FFF" >
+                <div class="col-lg-12" >
+                    <ul class="nav nav-tabs">
+                        <li class="active">
+                            <a href="#">HOME</a></li>
+                        <li><a href="#">PRODUTOS</a></li>
+                        <li><a href="#">CARRINHO DE COMPRAS</a></li>
+                        <li><a href="#">LOGIN</a></li>
+                        <li><a href="#">PERFIL</a></li>
+                    </ul>
+                </div>
             </div>
-        </div>
 
 
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="alert alert-success" role="alert" style="text-align:center " > <h3 style="color:orangered" >TEXTO TEXTO TEXTO TEXTO TEXTO </h3> </div>
-            </div>
-        </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <img src="img/direito.jpg" height="500" width="600"/>
 
-    </body>
-</html>
+                </div>
+                <div class="col-lg-6">
+                    <div class="alert alert-success" role="alert" style="text-align:justify " > <h4 style="color:orangered" <blockquote >
+
+                                <br>FUNDAMENTOS DO DIREITO <br style="text-align:justify ">
+
+                                <br>Em que se baseia a obrigatoriedade do Direito?<br>
+                                <br> O que é legal? É aquilo que é justo.<br>
+                                <br> O que é justo?<br>
+                                <br>Justiça é a virtude constante de dar a cada um o que é seu. Este é o fundamento mais atual do Direito. Trata-se da teoria da justiça adotada por Miguel Reale.<br>
+                                Inicialmente dizia-se que o direito era baseado na Divindade. Mais tarde,, desenvolveu-se a idéia de que o fundamento do Direito era a Razão, entendido como direito natural (jusnaturalismo). Para uns, era vontade do mais forte sobre a vontade dos mais fracos. Para outros , a aliança dos fracos contra o abuso da força.
+                                <br>Falava-se também, em ajuste de interesses ou exigência da ordem.<br>
+                                <br>Este conjunto de doutrina vem-se alterando através dos tempos.<br>
+                                <br>Atualmente surge a teoria da justiça.<br>
+
+                                <br>Direito objetivo<br>
+                                <br>Direito positivo<br>
+                                <br>Normas jurídicas<br>
+                                <br>Lei.<br>
+
+                                <br>Direito subjetivo
+                                Direitos da pessoa
+                                Referente ao sujeito
+                                O direito que a pessoa pode reivindicar para si.
+                                Todo direito subjetivo tem o ativo e passivo. Os direitos e os deveres.
+                                Ex. Eu tenho o direito de assistir aula perante o dever de pagar a faculdade. </h4
+                                > <br>
+                                </div>
+                                </div>
+                                </div>
+
+
+
+
+
+                                </div>
+                                </body>
+                                </html>
